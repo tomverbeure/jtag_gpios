61f912d0d2b2f61021ac155044b7276ec039ff47
Tom Verbeure <hombre+github@gmail.com>
2018-05-04 05:15:43 +0000

    Print switch result

--- openocd/target/ocd_rpc_example.py
+++ openocd/target/ocd_rpc_example.py
@@ -97,7 +97,7 @@ class OpenOcd:
 if __name__ == "__main__":
 
     def show(*args):
-        print(*args, end="")
+        print(*args, end="\n")
 
     with OpenOcd() as ocd:
         ocd.send("reset")
@@ -107,14 +107,18 @@ if __name__ == "__main__":
         show(ocd.send("ocd_gpios settings 0 1"))
         show(ocd.send("ocd_gpios settings 1 1"))
 
-        for _ in range(20):
-            show(ocd.send("ocd_gpios set_pin 0 1"))
-            show(ocd.send("ocd_gpios set_pin 1 0"))
-            show(ocd.send("ocd_sleep 100"))
-            show(ocd.send("ocd_gpios set_pin 0 0"))
-            show(ocd.send("ocd_gpios set_pin 1 1"))
-            show(ocd.send("ocd_sleep 100"))
+        prev_switch_value = -1;
 
-        value = ocd.send("ocd_gpios get_pin 0")
-        print(value)
+        while True:
+            ocd.send("ocd_gpios set_pin 0 1")
+            ocd.send("ocd_gpios set_pin 1 0")
+            ocd.send("ocd_sleep 100")
+            ocd.send("ocd_gpios set_pin 0 0")
+            ocd.send("ocd_gpios set_pin 1 1")
+            ocd.send("ocd_sleep 100")
+
+            switch_value = ocd.send("ocd_gpios get_pin 2")
+            if switch_value != prev_switch_value:
+                print("Switch: %s" % switch_value, end='')
+                prev_switch_value = switch_value
             
