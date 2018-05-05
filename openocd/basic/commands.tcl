6a13ffc6d1fa1c9cfed754c0bcdc47130315d41f
Tom Verbeure <hombre+github@gmail.com>
2018-05-05 05:20:41 +0000

    Adapt basic example to latest JTAG_GPIO block

--- openocd/basic/commands.tcl
+++ openocd/basic/commands.tcl
@@ -1,30 +1,38 @@
 
-jtag init
-
 # Force TAP to get back to Test Logic Reset state
 runtest 5
 
-# Select GPIO_CONFIG IR
-irscan chip.gpios_tap 0x5 -endstate IRPAUSE
+# Select SCAN_N IR
+irscan chip.gpios_tap 0x2 -endstate IRPAUSE
+
+# Select CONFIG register
+drscan chip.gpios_tap 1 0x0
+
+# Select EXTEST IR
+irscan chip.gpios_tap 0x3 -endstate IRPAUSE
 
 # Set all GPIOs to output
-drscan chip.gpios_tap 3 0x7
+drscan chip.gpios_tap 4 0xf
 
-# Select GPIO_DATA IR
-irscan chip.gpios_tap 0x4 -endstate IRPAUSE
+# Select SCAN_N IR
+irscan chip.gpios_tap 0x2 -endstate IRPAUSE
 
-# Set all GPIOs to high
-drscan chip.gpios_tap 3 0x7
-sleep 250
+# Select DATA register
+drscan chip.gpios_tap 1 0x1
 
-# Set all GPIOs to high
-drscan chip.gpios_tap 3 0x0
-sleep 250
+# Select EXTEST IR
+irscan chip.gpios_tap 0x3 -endstate IRPAUSE
 
 # Set all GPIOs to high
-drscan chip.gpios_tap 3 0x7
+drscan chip.gpios_tap 4 0xf
 sleep 250
 
-# Set all GPIOs to high
-drscan chip.gpios_tap 3 0x0
-sleep 250
+for {set i 0} {$i < 20} {incr i} {
+    # Set all GPIO 0 to 1 and GPIO to 0
+    drscan chip.gpios_tap 4 0x9
+    sleep 250
+
+    # Set all GPIO 0 to 0 and GPIO to 1
+    drscan chip.gpios_tap 4 0xa
+    sleep 250
+}
